e2f85e18806de4fffeee4e76ad8b1353cef6ae8d
Dave Mateer <davemateer@gmail.com>
2026-03-05 19:59:22 +0000

    add sumMultiples function and corresponding tests

---
+++ euler.test.js
@@ -0,0 +1,5 @@
+import { sumMultiples } from "./euler.js";
+
+console.assert(sumMultiples(10) === 23, "Expected 23 for limit 10");
+console.assert(sumMultiples(1000) === 233168, "Expected 233168 for limit 1000");
+console.log("All tests passed");
